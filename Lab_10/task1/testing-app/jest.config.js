2e1463902d1106e7f79601a54329865efb3444ca
Just2Alim <2019_alim@mail.ru>
2026-03-24 23:02:30 +0500

    add lab10

---
+++ Lab_10/task1/testing-app/jest.config.js
@@ -0,0 +1,12 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }]
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx']
+};
